d47d135285bccd259258e7c8e883b3d05d764a32
klinsmanndennis <klinsmanntje@yahoo.co.id>
2020-02-24 13:16:26 -0600

    milestone 2

--- app/src/main/java/com/example/lab4milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone2/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -16,7 +15,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Telephony;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -29,26 +27,14 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
-
     LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(Build.VERSION.SDK_INT >= 23 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-
-        else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location != null) {
-                updateLocationInfo(location);
-            }
-        }
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
@@ -70,64 +56,71 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+        if (Build.VERSION.SDK_INT >= 23 && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        if (location != null) {
+            updateLocationInfo(location);
+        }
     }
 
-    public void startListening(){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+    public void startListening() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         }
     }
 
-
-    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             startListening();
         }
     }
 
     public void updateLocationInfo(Location location) {
         Log.i("LocationInfo", location.toString());
-        TextView latTextView = (TextView) findViewById(R.id.latitude);
-        TextView lonTextView = (TextView) findViewById(R.id.longitude);
-
+        TextView latTextView = (TextView) findViewById(R.id.latitudeText);
+        TextView longTextView = (TextView) findViewById(R.id.longitudeText);
+        TextView altiTextView = (TextView) findViewById(R.id.altitudeText);
+        TextView accTextView = (TextView) findViewById(R.id.accuracyText);
         latTextView.setText("Latitude: " + location.getLatitude());
-        lonTextView.setText("Longitude: " + location.getLongitude());
-
+        longTextView.setText("Longtitude : " + location.getLongitude());
+        altiTextView.setText("Altitude : " + location.getAltitude());
+        accTextView.setText("Accuracy : " + location.getAccuracy());
         Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-
         try {
-            String address = "Could not find address";
-            List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-
-            if(listAddresses != null && listAddresses.size() > 0) {
-
-                Log.i("PlaceInfo", listAddresses.get(0).toString());
+            String address = "Could not find Address";
+            List<Address> listAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (listAddress != null && listAddress.size() > 0) {
+                Log.i("PlaceInfo", listAddress.get(0).toString());
                 address = "Address: \n";
-
-                if(listAddresses.get(0).getSubThoroughfare() != null) {
-                    address += listAddresses.get(0).getSubThoroughfare() + " ";
+                if (listAddress.get(0).getSubThoroughfare() != null) {
+                    address += listAddress.get(0).getSubThoroughfare() + " ";
                 }
-                if(listAddresses.get(0).getThoroughfare() != null) {
-                    address += listAddresses.get(0).getThoroughfare() + "\n";
-                }
-                if(listAddresses.get(0).getLocality() != null) {
-                    address += listAddresses.get(0).getLocality() + "\n";
-                }
-                if(listAddresses.get(0).getPostalCode() != null) {
-                    address += listAddresses.get(0).getPostalCode() + "\n";
-                }
-                if(listAddresses.get(0).getCountryName() != null) {
-                    address += listAddresses.get(0).getCountryName() + "\n";
+                if (listAddress.get(0).getThoroughfare() != null) {
+                    address += listAddress.get(0).getThoroughfare() + " ";
                 }
 
+                if (listAddress.get(0).getLocality() != null) {
+                    address += listAddress.get(0).getLocality() + " ";
+                }
+                if (listAddress.get(0).getPostalCode() != null) {
+                    address += listAddress.get(0).getPostalCode() + " ";
+                }
+                if (listAddress.get(0).getCountryName() != null) {
+                    address += listAddress.get(0).getCountryName() + " ";
+                }
             }
-
-            TextView addressTextView = (TextView) findViewById(R.id.addressTextView);
+            TextView addressTextView = (TextView) findViewById(R.id.addressText);
             addressTextView.setText(address);
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
